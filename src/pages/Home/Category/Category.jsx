aeb107a6a0641b199c3e6a95b37bcae3b0ba7fc4
Md-Azad <sokalazad@gmail.com>
2023-06-13 22:40:16 +0200

    added a components named sectionTitle for setting dynamic titles

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,11 +8,17 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
     return (
      
-      <Swiper
+      <section>
+        <SectionTitle
+        subHeading={"From 10am to 11pm"}
+        heading={"Order Online"}
+        ></SectionTitle>
+        <Swiper
         slidesPerView={4}
         spaceBetween={65}
         pagination={{
@@ -43,6 +49,7 @@ const Category = () => {
         </SwiperSlide>
         
       </Swiper>
+      </section>
     
     );
 };
