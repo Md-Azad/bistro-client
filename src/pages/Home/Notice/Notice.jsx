613ec5ab4b18f78e9657b9ea791dc95bf91a3ba1
Md-Azad <sokalazad@gmail.com>
2023-06-13 22:47:57 +0200

    todo: notice part will be done later

---
+++ src/pages/Home/Notice/Notice.jsx
@@ -0,0 +1,11 @@
+
+
+const Notice = () => {
+    return (
+        <div className="h-72 my-8 bg-slate-200 flex items-center justify-center">
+            <h1 className="text-3xl  ">Todo: Here will be a simple notice page as like as figma.</h1>
+        </div>
+    );
+};
+
+export default Notice;
