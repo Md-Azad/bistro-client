31af77dde8bf55b2379860e8641118cb220a7411
Md-Azad <sokalazad@gmail.com>
2023-06-16 00:54:00 +0200

    added a section named cover and used react blur parallax

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,9 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+
 
 
 const Menu = () => {
@@ -7,7 +12,8 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
-            <h3>menu</h3>
+            <Cover img={menuImg} title="Our menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
