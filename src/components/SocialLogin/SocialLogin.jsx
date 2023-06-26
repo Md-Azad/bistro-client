d9c6344ce38ff039e64b68e61f106d04b9128ad8
Md-Azad <sokalazad@gmail.com>
2023-06-27 00:55:06 +0200

    added user to database only after first time login

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,10 +1,16 @@
 import { useContext } from 'react';
 import { FaGoogle } from 'react-icons/fa';
 import { AuthContext } from '../../providers/AuthProvider';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const SocialLogin = () => {
     const {googleSignIn} = useContext(AuthContext);
+
+    let navigate = useNavigate();
+    let location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
  
 
     const hangleGoogleLogin =()=>{
@@ -12,6 +18,23 @@ const SocialLogin = () => {
         .then(result=>{
             const loggedUser = result.user;
             console.log(loggedUser);
+
+            const saveUser ={name: loggedUser.displayName, email:loggedUser.email}   
+                fetch('http://localhost:5000/users',{
+                  method: 'POST',
+                  headers:{
+                    'content-type': 'application/json'
+                  },
+                  body: JSON.stringify(saveUser)
+                })
+                .then(res=>res.json())
+                .then(()=>{
+                    navigate(from, { replace: true });
+                })
+
+
+            
+            
         })
     }
   return (
