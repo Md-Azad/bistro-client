b23aec7f1f66d34b3f6eacee819d8a7241ee27a4
Md-Azad <sokalazad@gmail.com>
2023-06-29 00:45:55 +0200

    added users in table format

--- src/pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,11 +1,68 @@
-
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
 
 const AllUsers = () => {
-    return (
-        <div>
-                <h1>All users will be displayed here.</h1>
-        </div>
-    );
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await fetch("http://localhost:5000/users");
+    return res.json();
+  });
+
+  const handleDelete = user=>{
+
+  }
+
+  return (
+    <div>
+      <Helmet>
+        <title>bistro boss || all users</title>
+      </Helmet>
+      <h1 className="font-semibold text-3xl">total users: {users.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index +1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                    {
+                        user.role === 'admin'? 'admin': <button
+                        
+                        className="btn btn-ghost  bg-orange-600 text-white"
+                      >
+                        <FaUserShield></FaUserShield>
+                      </button>
+                    }
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user)}
+                    className="btn btn-ghost  bg-red-600 text-white"
+                  >
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
